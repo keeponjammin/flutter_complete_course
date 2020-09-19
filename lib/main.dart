846afc07fb65c5ee9a463e78b44db3ccbf797326
Jeroen Serdijn <jeroen_serdijn@hotmail.com>
2020-09-19 19:36:46 +0200

    2.25 added stateful widget with question update

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,51 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      questionIndex++;
+    });
+    print(questionIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
+    var quizQuestions = [
+      'Wat is het beste huisdier?',
+      'Wat is de beste plek om games te spelen?',
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Mijn eerste applicatie'),
         ),
-        body: Text('Dit is mijn eerste app!'),
+        body: Column(
+          children: [
+            Text(quizQuestions[questionIndex]),
+            RaisedButton(
+              child: Text('Antwoord 1'),
+              onPressed: () => print('Antwoord 1 gekozen!'),
+            ),
+            RaisedButton(
+              child: Text('Antwoord 2'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Antwoord 3'),
+              onPressed: answerQuestion,
+            ),
+          ],
+        ),
       ),
     );
   }
