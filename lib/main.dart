3a48374a5371fcddd5222b2cb8f810bec823b6db
Jeroen Serdijn <jeroen_serdijn@hotmail.com>
2020-09-21 17:52:53 +0200

    added check if done and added some print statements

--- lib/main.dart
+++ lib/main.dart
@@ -15,48 +15,55 @@ class MyApp extends StatefulWidget {
 }
 
 class MyAppState extends State<MyApp> {
+  final quizQuestions = const [
+    {
+      'questionText': 'Wat is het beste huisdier?',
+      'answers': ['Cavia', 'Kat', 'Hond', 'Olifant']
+    },
+    {
+      'questionText': 'Wat is de beste programmeertaal?',
+      'answers': ['Dart', 'C#', 'PHP', 'JavaScript']
+    },
+    {
+      'questionText': 'Wie kan het niet laten mij te onderbreken tijdens werk?',
+      'answers': ['Martine', 'Martine', 'Martine', 'Martine']
+    },
+  ];
   var _questionIndex = 0;
 
   void _answerQuestion() {
     setState(() {
       _questionIndex++;
     });
-    //print(_questionIndex);
+    if (_questionIndex < quizQuestions.length) {
+      print('We have more questions!');
+    } else {
+      print('We zijn klaar!');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    var quizQuestions = [
-      {
-        'questionText': 'Wat is het beste huisdier?',
-        'answers': ['Cavia', 'Kat', 'Hond', 'Olifant']
-      },
-      {
-        'questionText': 'Wat is de beste programmeertaal?',
-        'answers': ['Dart', 'C#', 'PHP', 'JavaScript']
-      },
-      {
-        'questionText':
-            'Wie kan het niet laten mij te onderbreken tijdens werk?',
-        'answers': ['Martine', 'Martine', 'Martine', 'Martine']
-      },
-    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Super Sexy Jeroen App'),
         ),
-        body: Column(
-          children: [
-            Question(
-              quizQuestions[_questionIndex]['questionText'],
-            ),
-            ...(quizQuestions[_questionIndex]['answers'] as List<String>)
-                .map((answer) {
-              return Answer(_answerQuestion, answer);
-            }).toList(),
-          ],
-        ),
+        body: _questionIndex < quizQuestions.length
+            ? Column(
+                children: [
+                  Question(
+                    quizQuestions[_questionIndex]['questionText'],
+                  ),
+                  ...(quizQuestions[_questionIndex]['answers'] as List<String>)
+                      .map((answer) {
+                    return Answer(_answerQuestion, answer);
+                  }).toList(),
+                ],
+              )
+            : Center(
+                child: Text('Goed gedaan!'),
+              ),
       ),
     );
   }
